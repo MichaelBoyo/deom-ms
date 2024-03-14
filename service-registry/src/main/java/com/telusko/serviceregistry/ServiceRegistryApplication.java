de4461fc056e3de12af13a56acef743dc32e7e8a
Michael Boyo <boyomichaelbidemi@gmail.com>
2024-03-14 23:51:59 +0100

    service registry

--- service-registry/src/main/java/com/telusko/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/telusko/serviceregistry/ServiceRegistryApplication.java
@@ -3,13 +3,25 @@ package com.telusko.serviceregistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
 public class ServiceRegistryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceRegistryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistryApplication.class, args);
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<?> test() {
+        return ResponseEntity.ok(Map.of("200 ok", "QuestionServiceApplication is working"));
+    }
+
 
 }
