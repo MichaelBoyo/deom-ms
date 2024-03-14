551c18973ed58ab1fa2282843d825c60b425d7c7
Michael Boyo <boyomichaelbidemi@gmail.com>
2024-03-14 23:44:55 +0100

    service registry

--- api-gateway/src/main/java/com/telusko/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/telusko/apigateway/ApiGatewayApplication.java
@@ -2,13 +2,23 @@ package com.telusko.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @SpringBootApplication
+@RestController
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	@GetMapping("/health")
+	public ResponseEntity<?> test(){
+		return ResponseEntity.ok(Map.of("200 ok", "Api Gateway is working"));
 	}
 
 }
--- question-service/src/main/java/com/telusko/questionservice/QuestionServiceApplication.java
+++ question-service/src/main/java/com/telusko/questionservice/QuestionServiceApplication.java
@@ -2,13 +2,24 @@ package com.telusko.questionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @SpringBootApplication
+@RestController
 public class QuestionServiceApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(QuestionServiceApplication.class, args);
 	}
+		@GetMapping("/health")
+	public ResponseEntity<?> test(){
+		return ResponseEntity.ok(Map.of("200 ok", "QuestionServiceApplication is working"));
+	}
+
 
 }
