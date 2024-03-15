0c70f2105e86b71f34758807164f2d5285e27095
Michael Boyo <boyomichaelbidemi@gmail.com>
2024-03-15 14:43:08 +0100

    Update FeignClient URL in QuizInterface

--- quiz-service/src/main/java/com/telusko/quizservice/feign/QuizInterface.java
+++ quiz-service/src/main/java/com/telusko/quizservice/feign/QuizInterface.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient("QUESTION-SERVICE")
+@FeignClient("http://microservice-lb-tf-617656823.us-west-2.elb.amazonaws.com")
 public interface QuizInterface {
     @GetMapping("question/generate")
     public ResponseEntity<List<Integer>> getQuestionsForQuiz
